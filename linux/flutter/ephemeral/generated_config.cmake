28806fb5fec0af6a19e0524cc2203c17f7c61cce
ryiianqueiroz <112638880+ryiianqueiroz@users.noreply.github.com>
2023-03-10 21:14:58 -0300

    Add files via upload

---
+++ linux/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,21 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "/home/familia/snap/flutter/common/flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "/home/familia/Documentos/ryan/exe/Dart  !  Flutter/Flutter/04 - Calculadora" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=/home/familia/snap/flutter/common/flutter"
+  "PROJECT_DIR=/home/familia/Documentos/ryan/exe/Dart  !  Flutter/Flutter/04 - Calculadora"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=/home/familia/Documentos/ryan/exe/Dart  !  Flutter/Flutter/04 - Calculadora/.dart_tool/package_config.json"
+  "FLUTTER_TARGET=/home/familia/Documentos/ryan/exe/Dart  !  Flutter/Flutter/04 - Calculadora/lib/main.dart"
+)
